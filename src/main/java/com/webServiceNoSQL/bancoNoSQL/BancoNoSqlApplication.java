247310eec7d66004f3907ea8fbeebc732f816351
João Antonio <joao.vison.ae@gmail.com>
2025-04-22 16:11:24 -0300

    Primeiro commit do projeto com banco de dados NoSQL

---
+++ src/main/java/com/webServiceNoSQL/bancoNoSQL/BancoNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.webServiceNoSQL.bancoNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webServiceNoSQL/bancoNoSQL/BancoNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webServiceNoSQL.bancoNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
