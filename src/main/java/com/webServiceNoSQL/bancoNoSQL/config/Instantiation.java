9d66118de87855dda1da94924ebc22fe65f3cbfe
João Antonio <joao.vison.ae@gmail.com>
2025-04-28 16:06:25 -0300

    Adicionando comentarios ao post

--- src/main/java/com/webServiceNoSQL/bancoNoSQL/config/Instantiation.java
+++ src/main/java/com/webServiceNoSQL/bancoNoSQL/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.webServiceNoSQL.bancoNoSQL.config;
 import com.webServiceNoSQL.bancoNoSQL.domain.Post;
 import com.webServiceNoSQL.bancoNoSQL.domain.User;
 import com.webServiceNoSQL.bancoNoSQL.dto.AuthorDTO;
+import com.webServiceNoSQL.bancoNoSQL.dto.CommentDTO;
 import com.webServiceNoSQL.bancoNoSQL.repository.PostRepository;
 import com.webServiceNoSQL.bancoNoSQL.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","vou viajar para São Paulo", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
