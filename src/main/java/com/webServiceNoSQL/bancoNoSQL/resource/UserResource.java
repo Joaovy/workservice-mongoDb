6513d49bdfbe25f139318d2f177fce8c2ddafd48
João Antonio <joao.vison.ae@gmail.com>
2025-04-28 13:45:51 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/webServiceNoSQL/bancoNoSQL/resource/UserResource.java
+++ src/main/java/com/webServiceNoSQL/bancoNoSQL/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.webServiceNoSQL.bancoNoSQL.resource;
 
+import com.webServiceNoSQL.bancoNoSQL.domain.Post;
 import com.webServiceNoSQL.bancoNoSQL.domain.User;
 import com.webServiceNoSQL.bancoNoSQL.dto.UserDTO;
 import com.webServiceNoSQL.bancoNoSQL.service.UserService;
@@ -67,5 +68,12 @@ public class UserResource {
 
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPost());
+    }
+
 
 }
